1674022e744f91e9c04ef3718adc2659d139e7bf
Matteo Piffer <>
2020-03-15 20:01:14 +0100

    capitolo 9

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,28 +1,18 @@
 <template> <!-- HTML - structure-->
-    <router-link class="event-link" :to="{ name: 'event-show',params:{id:'1'}}">
+    <router-link class="event-link" :to="{ name: 'event-show',params:{ id: event.id }}">
         <div class="event-card -shadow">
             <span class="eyebrow" >@{{ event.time}} on {{event.date}}</span>
             <h4 class="title"> {{ event.title}}</h4>
+            <BaseIcon name="users">{{ event.attendees.length}} attending</BaseIcon><!--global component with slot-->
         </div>
     </router-link>
 </template>
 
 <script>/*-- JS - brain --*/
 export default {
-   data(){
-       return{
-           event:{
-               id:1,
-               title:'Beach Cleanup',
-               date:'Tues Aug 19 2018',
-               time:'6:00',
-               attendees:[
-                   {id: 'abc123', name: 'Adam Jahr'},
-                   {id: 'def456', name: 'Gregg Pollck'}
-               ]
-           }
-       }
-   } 
+   props:{
+     event:Object
+   }
 }
 </script>
 
